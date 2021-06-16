55e905b459c577f3c5265fdbbb78db26b2e96c36
rahinic <rahini.c@gmail.com>
2021-06-17 00:27:38 +0200

    clean-up

--- notebooks/trainConLL2003.py
+++ notebooks/trainConLL2003.py
@@ -25,12 +25,14 @@ print("All datasets successfully loaded!")
 
 
 ##########################################################################################
+
 ds = SlidingWindowDataset("C:/Users/rahin/projects/paper-draft-03/data/raw/ConLL2003-bioes-valid.txt")
 #ds = SlidingWindowDataset()
 x1, x2, y = ds.load_dictionaries()
 print(f"Length of vocabulary is: {len(x1)} and Length of POS table is: {len(x2)}")
 print(f"Length of Target look-up table is: {len(y)}")
 ################################# 01.Model Parameters ####################################
+
 # read this seq2seq model: https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html --> for understanding embedding dimension and output dimension  
 VOCAB_SIZE = len(x1)+len(x2)+2
 EMBED_DIM = 50
@@ -38,11 +40,12 @@ HIDDEN_DIM = 32
 NUM_LAYERS = 2
 NUM_OF_CLASSES = len(y)+1
 EPOCHS = 5
-LEARNING_RATE = 0.5
+LEARNING_RATE = 0.2
 BATCH_SIZE = 64
 
 print(f"Our vocab size to the model is therefore: {VOCAB_SIZE}")
 ################################### 02. NN Model  ########################################
+
 print("Step 02. builing the model...")
 model = RNNBIOESTagger(embedding_dimension= EMBED_DIM,
                             vocabulary_size=VOCAB_SIZE,
@@ -55,11 +58,11 @@ print("Done! here is our model:")
 print(model)
 print("----------------------------------------------------------------")
 ############################# 03. Optimizer and Loss  #################################
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu') 
-# optimizer = optim.Adam(model.parameters())
 optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
 criterion = nn.CrossEntropyLoss()
-#criterion = nn.KLDivLoss(reduction='batchmean')
+
 
 
 #define metric
@@ -70,7 +73,7 @@ def binary_accuracy(preds, y):
 
     # correct = (rounded_preds == y).float() 
 
-    _,pred_label = torch.max(rounded_preds, dim=0)
+    _,pred_label = torch.max(rounded_preds[-1], dim=0)
     correct = (pred_label == y).float()
     acc = correct.sum() / len(correct)
     return acc
@@ -107,7 +110,7 @@ def train(model, dataset, optimizer, criterion):
     #    predicted_labels = predicted_labels.to(torch.float)
     #    current_labels = current_labels.to(torch.float)
        loss = criterion(predicted_labels, current_labels)
-       accuracy = binary_accuracy(predicted_labels, current_labels)
+    #    accuracy = binary_accuracy(predicted_labels, current_labels)
 
        loss.backward()
        optimizer.step()
@@ -144,13 +147,13 @@ def evaluate(model, dataset, criterion):
             epoch_accuracy = 0
 
             epoch_loss += loss.item()
-            #epoch_accuracy += accuracy.item()
+            # epoch_accuracy += accuracy.item()
 
     return epoch_loss/len(dataset), epoch_accuracy/len(dataset)
 
 ############################################################################################
 ################################## 06. NN Model training #####################################
-N_EPOCHS = 3
+N_EPOCHS = 6
 best_valid_loss = float('inf')
 
 for epoch in range(N_EPOCHS):
